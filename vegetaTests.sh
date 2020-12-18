9d04e4b4d26b23f3413d7609e11de3394f5412a2
mrusso <mario.russo@lastminute.com>
2020-12-19 00:09:27 +0100

    remove duplication

--- vegetaTests.sh
+++ vegetaTests.sh
@@ -5,9 +5,9 @@ if ! [ -x "$(command -v vegeta)" ]; then
   exit 1
 fi
 
-singleTestDuration=90s
-rate=500
-sleepAfterTestGroup=15s
+singleTestDuration=3s
+rate=2000
+sleepAfterTestGroup=2s
 numberOfSingleTestRepetitions=3
 
 runSingleVegetaTest() {
@@ -24,7 +24,41 @@ runVegeta() {
   done
 }
 
-for path in blockingRestTemplate suspendingPureCoroutines suspendingFuelCoroutines webfluxPureReactive webfluxReactiveCoroutines; do
-  echo ${path}
-  runVegeta ${path} ${rateForNonBlocking} | sort -r | cut -c 42- | sed 's/ms//g; s/s//g; s/%//g; s/,//g'
-done
+runSingleVegetaTestWithPort() {
+  path=$1
+  port=$2
+
+  echo "GET http://localhost:${port}/gingerbread/${path}" | vegeta attack -timeout=5s -duration=${singleTestDuration} -rate ${rate} | vegeta report | egrep "^Requests|^Latencies|^Success"
+  sleep ${sleepAfterTestGroup}
+}
+
+runVegetaWithPort() {
+  path=$1
+  port=$2
+
+  for i in $(seq 1 $numberOfSingleTestRepetitions); do
+    runSingleVegetaTestWithPort ${path} ${port}
+  done
+}
+
+#for path in blockingRestTemplate suspendingPureCoroutines suspendingFuelCoroutines webfluxPureReactive webfluxReactiveCoroutines; do
+#  echo ${path}
+#  #runVegeta ${path} ${rateForNonBlocking} | sort -r | cut -c 42- | sed 's/ms//g; s/s//g; s/%//g; s/,//g'
+#  runVegeta ${path} ${rateForNonBlocking} > /tmp/result_${path}.txt
+#done
+
+#for path in quarkus_gingerbread; do
+#  echo ${path}
+#  #runVegeta ${path} ${rateForNonBlocking} | sort -r | cut -c 42- | sed 's/ms//g; s/s//g; s/%//g; s/,//g'
+#  runVegeta ${path} ${rateForNonBlocking} > /tmp/result_${path}.txt
+#done
+
+
+#runVegetaWithPort blockingRestTemplate 8080 > /tmp/result_blockingRestTemplate.txt
+#runVegetaWithPort suspendingPureCoroutines 8080 > /tmp/result_suspendingPureCoroutines.txt
+#runVegetaWithPort suspendingFuelCoroutines 8080 > /tmp/result_suspendingFuelCoroutines.txt
+#runVegetaWithPort webfluxPureReactive 8080 > /tmp/result_webfluxPureReactive.txt
+#runVegetaWithPort webfluxReactiveCoroutines 8080 > /tmp/result_webfluxReactiveCoroutines.txt
+runVegetaWithPort quarkus_gingerbread 8083 > /tmp/result_quarkus_gingerbread.txt
+
+# runblockingRestTemplate
