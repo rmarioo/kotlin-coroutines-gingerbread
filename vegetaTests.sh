05b7f878ad391dddc3e8762a83d1369c21d5e4f7
mrusso <mario.russo@lastminute.com>
2020-12-22 23:31:39 +0100

    wip

--- vegetaTests.sh
+++ vegetaTests.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v vegeta)" ]; then
   exit 1
 fi
 
-singleTestDuration=3s
+singleTestDuration=60s
 rate=2000
-sleepAfterTestGroup=2s
-numberOfSingleTestRepetitions=3
+sleepAfterTestGroup=30s
+numberOfSingleTestRepetitions=2
 
 runSingleVegetaTest() {
   path=$1
@@ -28,8 +28,12 @@ runSingleVegetaTestWithPort() {
   path=$1
   port=$2
 
+  echo "running http://localhost:${port}/gingerbread/${path}"
   echo "GET http://localhost:${port}/gingerbread/${path}" | vegeta attack -timeout=5s -duration=${singleTestDuration} -rate ${rate} | vegeta report | egrep "^Requests|^Latencies|^Success"
+  echo "completed http://localhost:${port}/gingerbread/${path} sleep for ${sleepAfterTestGroup}"
+  echo "sleeping  for ${sleepAfterTestGroup}"
   sleep ${sleepAfterTestGroup}
+  echo "awake"
 }
 
 runVegetaWithPort() {
@@ -54,11 +58,28 @@ runVegetaWithPort() {
 #done
 
 
-#runVegetaWithPort blockingRestTemplate 8080 > /tmp/result_blockingRestTemplate.txt
-#runVegetaWithPort suspendingPureCoroutines 8080 > /tmp/result_suspendingPureCoroutines.txt
-#runVegetaWithPort suspendingFuelCoroutines 8080 > /tmp/result_suspendingFuelCoroutines.txt
-#runVegetaWithPort webfluxPureReactive 8080 > /tmp/result_webfluxPureReactive.txt
-#runVegetaWithPort webfluxReactiveCoroutines 8080 > /tmp/result_webfluxReactiveCoroutines.txt
-runVegetaWithPort quarkus_gingerbread 8083 > /tmp/result_quarkus_gingerbread.txt
+
+#for rate in 2000; do
+#  for path in quarkus_gingerbread webfluxPureReactive; do
+#
+#    defaultPortTouse=8080
+#    quarkusPortTouse=8083
+#    if [ ${path} = "quarkus_gingerbread" ]; then
+#     portTouse=${quarkusPortTouse}
+#    else
+#     portTouse=${defaultPortTouse}
+#    fi
+#
+#    echo "run VegetaWithPort ${path} ${portTouse}";
+#    runVegetaWithPort ${path} ${portTouse}      > /tmp/result_${path}_rate_${rate}.txt
+#  done
+#done
+
+#echo "running blockingRestTemplate"; runVegetaWithPort blockingRestTemplate 8080      > /tmp/result_blockingRestTemplate_rate_${rate}.txt
+#echo "running suspendingPureCoroutines"; runVegetaWithPort suspendingPureCoroutines 8080  > /tmp/result_suspendingPureCoroutines_rate_${rate}.txt
+#echo "running suspendingFuelCoroutines"; runVegetaWithPort suspendingFuelCoroutines 8080  > /tmp/result_suspendingFuelCoroutines_rate_${rate}.txt
+#echo "running webfluxReactiveCoroutines"; runVegetaWithPort webfluxReactiveCoroutines 8080 > /tmp/result_webfluxReactiveCoroutines_rate_${rate}.txt
+echo "running webfluxPureReactive"; runVegetaWithPort webfluxPureReactive 8080       > /tmp/result_webfluxPureReactive_rate_${rate}.txt
+echo "running quarkus_gingerbread"; runVegetaWithPort quarkus_gingerbread 8083       > /tmp/result_quarkus_gingerbread_rate_${rate}.txt
 
 # runblockingRestTemplate
